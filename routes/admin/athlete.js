ccc1ff9da566033a64ed01c4d2fc8470a73d9d57
faezeh <abdif701@gmail.com>
2025-10-27 19:25:51 +0200

    added functionality for multer of file uploading

--- routes/admin/athlete.js
+++ routes/admin/athlete.js
@@ -6,10 +6,20 @@ var passport = require("passport");
 var config = require("../../config");
 const validate = require("../../middleware/validate");
 const { check } = require("express-validator");
-
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './public/img/')
+  },
+  filename: function(req, file, cb) {
+   cb(null, new Date().toISOString() + file.originalname);
+  }
+})
+const upload = multer({storage: storage})
 
 router.post(
     "/admin/athlete",
+    upload.single("image"),
     passport.authenticate("jwt", { session: false }),
     //userPolicy.isAllowed,
     athleteController.create
